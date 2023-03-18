8d78281fdf51e5146abf508aa25d65f39da2bed6
Tom Hermans <thermans@positivethinking.tech>
2023-03-18 20:52:27 +0100

    add a page about

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- Nuxt automatically imports any components in your components/ directory -->
+  <div class="text-secondary-darker dark:text-white bg-white dark:bg-black">
+    <div
+      class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0"
+    >
+      <div class="max-w-screen-2xl grid grid-cols-2 mx-auto sm:px-6 lg:px-8">
+        <h1 class="text-red-600 font-bold text-3xl">About page</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <template>
   <!-- Nuxt automatically imports any components in your components/ directory -->
+  <NuxtLink to="/about">About page</NuxtLink>
   <NuxtDemo />
+  
 </template>
